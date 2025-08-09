329ccc1e93eb949f7487cf43f709817747569008
harshavardan423 <65705089+harshavardan423@users.noreply.github.com>
2025-08-09 21:48:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ import razorpay
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
 
